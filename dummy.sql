32c5d405fc5958f08d58251d2847e9f6c82bbc89
Oracle User <oracle@spinazie.bos>
2017-01-09 21:06:04 +0100

    Added settings.sql to be executed

--- dummy.sql
+++ dummy.sql
@@ -1,6 +1,8 @@
--- Name   : dummy.sql
--- Author : Martijn Bos
--- Input  : -
--- Description : Just a small dummy of which other files can be created
+-- Name        : dummy.sql
+-- Author      : Martijn Bos
+-- Input       : -
+-- Description : Just a dummy, used as an example for new files
 -- ---------------------------------------------------------------------
 -- 2017-01-09 : MB : Initial Version
+
+@settings.sql
