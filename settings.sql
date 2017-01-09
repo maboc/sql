58ecd0f3829013b32db9351c3663fb7acb2cd0dd
Oracle User <oracle@spinazie.bos>
2017-01-09 21:06:23 +0100

    first commit for this file

---
+++ settings.sql
@@ -0,0 +1,9 @@
+-- Name        : settings.sql
+-- Author      : Martijn Bos
+-- Input       : -
+-- Description : All commons settings I like for sqlplus to work
+-- ---------------------------------------------------------------------
+-- 2017-01-09 : MB : Initial Version
+
+set pages 100
+set lines 200
