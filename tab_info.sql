8b7b4b6c1f140dce45ad6a0aa219e0c09e210319
Oracle User <oracle@spinazie.bos>
2017-01-10 21:19:15 +0100

    table info added and adjusted some settings for that

---
+++ tab_info.sql
@@ -0,0 +1,43 @@
+-- Name        : tab_info.sql
+-- Author      : Martijn Bos
+-- Input       : Owner/schema
+--               Table
+-- Description : Show information about a table and it's indexes
+-- ---------------------------------------------------------------------
+-- 2017-01-10 : MB : Initial Version
+
+@settings.sql
+
+accept owner prompt 'Owner : '
+accept table prompt 'Table : '
+
+select owner,
+       table_name,
+       num_rows,
+       avg_row_len,
+       status,
+       last_analyzed,
+       blocks,
+       empty_blocks,
+       avg_space
+from   dba_tables 
+where  lower(owner)=lower('&&owner') 
+       and lower(table_name)=lower('&&table');
+
+select owner,
+       index_name,
+       index_type,
+       uniqueness,
+       blevel,
+       leaf_blocks,
+       distinct_keys,
+       avg_leaf_blocks_per_key albpk,
+       avg_data_blocks_per_key adbpk,
+       clustering_factor cl_fac,
+       status,
+       num_rows,
+       last_analyzed
+from   dba_indexes
+where  lower(table_owner)=lower('&&owner')
+       and lower(table_name)=lower('&&table')
+/
