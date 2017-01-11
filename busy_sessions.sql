f04799843decefdaea5896879883cf422184827a
Martijn Bos <martijn@dtp13212.org.ads.hhs.nl>
2017-01-11 07:34:28 +0100

    busy_sessions.sql is toegevoegd

---
+++ busy_sessions.sql
@@ -0,0 +1,8 @@
+-- Name        : busy_sessions.sql
+-- Author      : Martijn Bos
+-- Input       : -
+-- Description : Which sessions have the most waitevents in the last 5 minutes (ASH)
+-- ---------------------------------------------------------------------
+-- 2017-01-11 : MB : Initial Version
+
+@settings.sql
