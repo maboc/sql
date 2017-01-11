ed0ca0c25caf5d8b4e83eb6817e7c72963d6eb60
Martijn Bos <a.m.bos@hhs.nl>
2017-01-11 08:33:38 +0100

    The help system is being created

---
+++ help.sql
@@ -0,0 +1,10 @@
+-- Name        : help.sql
+-- Author      : Martijn Bos
+-- Input       : -
+-- Description : A list of all sqls and some explanantion
+-- ---------------------------------------------------------------------
+-- 2017-01-11 : MB : Initial Version
+
+@settings.sql
+
+!cat help.txt
