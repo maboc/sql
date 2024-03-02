c6b97d64448f237cf283c5d72b8af4e821b06360
Oracle Software Owner <oracle@bloemkool.bos>
2024-03-02 21:42:03 +0100

    diverse verbeteringen

--- oracle/cr_sg.sql
+++ oracle/cr_sg.sql
@@ -1,25 +1,24 @@
 create or replace type result_t is table of varchar2(250);
 /
 
-create or replace function sg_func (s1 in int, s2 in int, interval in int) 
-  return result_t 
-  pipelined
+create or replace function sg_func (s1 in number, s2 in number, interval in number) 
+  return result_t pipelined
 is
   type integer_t is table of int index by pls_integer;
   type number_t is table of number index by pls_integer;
-  memory1 integer_t;
-  memory2 integer_t;
+  memory1 number_t;
+  memory2 number_t;
   memoryq number_t;
-  memory_size int;
-  c int;
-  v1_new integer;
-  v2_new integer;
-  v1_old integer;
-  v2_old integer;
+  memory_size number;
+  c number;
+  v1_new number;
+  v2_new number;
+  v1_old number;
+  v2_old number;
   q_old number;
   q_new number;
-  delta1 integer;
-  delta2 integer;
+  delta1 number;
+  delta2 number;
   deltaq number;
   regel1 varchar2(250);
   regel2 varchar2(250);
@@ -27,24 +26,24 @@ is
   stat1 varchar2(50);
   stat2 varchar2(50);
   statq varchar2(100);
-  high1 int;
-  high2 int;
-  low1 int;
-  low2 int;
+  high1 number;
+  high2 number;
+  low1 number;
+  low2 number;
   highq number;
   lowq number;
-
-  block1 int; --block: size of a block in the graph
-  block2 int;
+  r result_t;
+  block1 number; --block: size of a block in the graph
+  block2 number;
   blockq number; 
-  gl int;  --gl: graph lines
-  gr int;
+  gl number;  --gl: graph lines
+  gr number;
 
-  function high (memory in integer_t)
-  return int
+  function high (memory in number_t)
+  return number
   is
-    c int;
-    h int;
+    c number;
+    h number;
   begin
     h:=memory(1);
     for c in 1..memory.count
@@ -57,11 +56,12 @@ is
 
   return h;
   end;
-  function low (memory in integer_t)
-  return int
+
+  function low (memory in number_t)
+  return number
   is
-    c int;
-    l int;
+    c number;
+    l number;
   begin
     l:=memory(1);
     for c in 1..memory.count
@@ -75,6 +75,33 @@ is
   return l;
   end;
 
+function initialize_memory(memory in out number_t, memory_size in number)
+return number_t
+is
+  c number;
+begin
+  for c in 1..memory_size
+  loop
+    memory(c):=1;
+  end loop;
+  
+  return memory;
+end;
+
+function fill_memory(memory in out number_t, value in number)
+return number_t
+is
+  c number;
+begin
+  for c in 1..memory.count-1
+  loop
+    memory(c):=memory(c+1);
+  end loop;
+  memory(memory.count):=value;
+
+  return memory;
+end;
+
 begin
   -- initialisation
   select name into stat1 from v$sysstat where statistic#=s1;
@@ -82,23 +109,12 @@ begin
   statq:=stat1||'/'||stat2;
   gl:=20; -- number of lines in a graph
   memory_size:=50;
-  
-  -- inititalize the arrays
-  for c in 1..memory_size
-  loop
-    memory1(c):=0;
-  end loop;
  
-  for c in 1..memory_size
-  loop
-    memory2(c):=0;
-  end loop;
-
-  for c in 1..memory_size
-  loop
-    memoryq(c):=0;
-  end loop;
-
+  -- inititalize the arrays
+  memory1:=initialize_memory(memory1, memory_size);
+  memory2:=initialize_memory(memory2, memory_size);
+  memoryq:=initialize_memory(memoryq, memory_size);
+ 
   loop
 
     -- get the latest values
@@ -107,55 +123,32 @@ begin
 
     -- put the values in the arrays
     delta1:=v1_new-v1_old;
-    for c in  1..memory_size-1
-    loop
-      memory1(c):=memory1(c+1);
-    end loop;
-
-    memory1(memory_size):=delta1;
+    memory1:=fill_memory(memory1, delta1);
 
     delta2:=v2_new-v2_old;
-    for c in  1..memory_size-1
-    loop
-      memory2(c):=memory2(c+1);
-    end loop;
+    memory2:=fill_memory(memory2, delta2);    
 
-    memory2(memory_size):=delta2;
-
-    deltaq:=round(delta1/delta2, 2);
-
-    for c in 1..memory_size-1
-    loop
-      memoryq(c):=memoryq(c+1);
-    end loop;
-    
-    memoryq(memory_size):=deltaq;
+    if (delta2=0)
+    then
+      deltaq :=0;
+    else
+      deltaq:=round(delta1/delta2, 2);
+    end if;
+    memoryq:=fill_memory(memoryq, deltaq);
 
     -- get the highest and lowest values in the arrays
     high1:=high(memory1);
     low1:=low(memory1);
 
-
     high2:=high(memory2);
     low2:=low(memory2);
 
-    highq:=memoryq(memory_size);
-    lowq:=memoryq(memory_size);
-    for c in 1..memory_size
-    loop
-      if (memoryq(c)>highq)
-      then
-        highq:=memoryq(c);
-      end if;
-      
-      if (memoryq(c)<lowq)
-      then
-        lowq:=memoryq(c);
-      end if;
-    end loop;
+    highq:=high(memoryq);
+    lowq:=low(memoryq);
 
     -- lets's draw the arrays
     -- 1st array
+
     pipe row ('------------ '|| stat1 ||' --------------');
     pipe row ('- Low  : '||low1);
     pipe row ('- High : '||high1);
@@ -182,7 +175,7 @@ begin
       pipe row (regel1);
     end loop;
 
-    pipe row ('-----------------------------------------------------');
+   pipe row ('-----------------------------------------------------');
 
     -- second array
     pipe row ('------------ '|| stat2 ||' --------------');
